6ddfdfe1014c7876d992530ec51c4fab16a1c69b
GitHub Action <action@github.com>
2022-01-26 08:56:06 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/crazy6995/intellijpluginkotlinbuilder/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.crazy6995.intellijpluginkotlinbuilder.services
+
+import com.github.crazy6995.intellijpluginkotlinbuilder.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
